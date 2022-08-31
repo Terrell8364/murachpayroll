8f44a8f5d56f3a0ffc9be3f4b114f957da8713b2
Terrell Robinson <terrell_robinson@insideranken.org>
2022-08-30 20:19:48 -0500

    Payroll console and GUI Chapters 1 - 3

---
+++ payrollConsole01/payrollConsole01/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace payrollConsole01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstname;
+            string lastname;
+            decimal hours;
+            decimal rate;
+            decimal gross;             //Gross = pay * hours
+
+            //  Input fistname, lastname, hours, rate
+            Console.Write("Input first name: ");
+            firstname = Console.ReadLine();
+
+            Console.Write("Input last name: ");
+            lastname = Console.ReadLine();
+
+            Console.Write("Input hours worked: ");
+            hours = Convert.ToDecimal(Console.ReadLine());
+
+            Console.Write("Input hourly rate: ");
+            rate = Convert.ToDecimal(Console.ReadLine());
+
+            //  Calculate gross pay
+            gross = hours * rate;
+
+            //  Output everything
+            Console.Clear();
+            Console.WriteLine("First Name: " + firstname);
+            Console.WriteLine("Last Name: " + lastname);
+            Console.WriteLine("Hrs Worked " + hours.ToString("n2"));
+            Console.WriteLine("Hrly Rate: " + rate.ToString("c"));
+            Console.WriteLine("Gross Pay: " + gross.ToString("c"));
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
---
+++ payrollConsole01/payrollGUI01/frmpayrollchapter1-3.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace payrollGUI01
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblFirstname = new System.Windows.Forms.Label();
+            this.lblLastname = new System.Windows.Forms.Label();
+            this.lblHours = new System.Windows.Forms.Label();
+            this.lblRate = new System.Windows.Forms.Label();
+            this.lblGross = new System.Windows.Forms.Label();
+            this.txtFirstname = new System.Windows.Forms.TextBox();
+            this.txtGross = new System.Windows.Forms.TextBox();
+            this.txtRate = new System.Windows.Forms.TextBox();
+            this.txtHours = new System.Windows.Forms.TextBox();
+            this.txtLastname = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(134, 439);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(174, 59);
+            this.btnCalculate.TabIndex = 4;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnClear.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(346, 439);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(198, 59);
+            this.btnClear.TabIndex = 5;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnExit.Location = new System.Drawing.Point(579, 439);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(200, 59);
+            this.btnExit.TabIndex = 6;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblFirstname
+            // 
+            this.lblFirstname.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFirstname.Location = new System.Drawing.Point(122, 23);
+            this.lblFirstname.Name = "lblFirstname";
+            this.lblFirstname.Size = new System.Drawing.Size(271, 66);
+            this.lblFirstname.TabIndex = 8;
+            this.lblFirstname.Text = "First Name:";
+            this.lblFirstname.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblLastname
+            // 
+            this.lblLastname.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLastname.Location = new System.Drawing.Point(150, 89);
+            this.lblLastname.Name = "lblLastname";
+            this.lblLastname.Size = new System.Drawing.Size(243, 57);
+            this.lblLastname.TabIndex = 9;
+            this.lblLastname.Text = "Last Name:";
+            this.lblLastname.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblHours
+            // 
+            this.lblHours.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHours.Location = new System.Drawing.Point(150, 161);
+            this.lblHours.Name = "lblHours";
+            this.lblHours.Size = new System.Drawing.Size(243, 57);
+            this.lblHours.TabIndex = 10;
+            this.lblHours.Text = "Hours:";
+            this.lblHours.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblRate
+            // 
+            this.lblRate.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRate.Location = new System.Drawing.Point(150, 237);
+            this.lblRate.Name = "lblRate";
+            this.lblRate.Size = new System.Drawing.Size(243, 57);
+            this.lblRate.TabIndex = 11;
+            this.lblRate.Text = "Rate:";
+            this.lblRate.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblGross
+            // 
+            this.lblGross.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGross.Location = new System.Drawing.Point(150, 322);
+            this.lblGross.Name = "lblGross";
+            this.lblGross.Size = new System.Drawing.Size(243, 57);
+            this.lblGross.TabIndex = 12;
+            this.lblGross.Text = "Gross:";
+            this.lblGross.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // txtFirstname
+            // 
+            this.txtFirstname.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFirstname.Location = new System.Drawing.Point(488, 32);
+            this.txtFirstname.Name = "txtFirstname";
+            this.txtFirstname.Size = new System.Drawing.Size(138, 42);
+            this.txtFirstname.TabIndex = 0;
+            // 
+            // txtGross
+            // 
+            this.txtGross.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtGross.Location = new System.Drawing.Point(488, 327);
+            this.txtGross.Name = "txtGross";
+            this.txtGross.ReadOnly = true;
+            this.txtGross.Size = new System.Drawing.Size(138, 42);
+            this.txtGross.TabIndex = 7;
+            // 
+            // txtRate
+            // 
+            this.txtRate.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtRate.Location = new System.Drawing.Point(488, 249);
+            this.txtRate.Name = "txtRate";
+            this.txtRate.Size = new System.Drawing.Size(138, 42);
+            this.txtRate.TabIndex = 3;
+            // 
+            // txtHours
+            // 
+            this.txtHours.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtHours.Location = new System.Drawing.Point(488, 169);
+            this.txtHours.Name = "txtHours";
+            this.txtHours.Size = new System.Drawing.Size(138, 42);
+            this.txtHours.TabIndex = 2;
+            // 
+            // txtLastname
+            // 
+            this.txtLastname.Font = new System.Drawing.Font("Arial", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLastname.Location = new System.Drawing.Point(488, 97);
+            this.txtLastname.Name = "txtLastname";
+            this.txtLastname.Size = new System.Drawing.Size(138, 42);
+            this.txtLastname.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.btnCalculate;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.CancelButton = this.btnClear;
+            this.ClientSize = new System.Drawing.Size(1004, 585);
+            this.Controls.Add(this.txtLastname);
+            this.Controls.Add(this.txtHours);
+            this.Controls.Add(this.txtRate);
+            this.Controls.Add(this.txtGross);
+            this.Controls.Add(this.txtFirstname);
+            this.Controls.Add(this.lblGross);
+            this.Controls.Add(this.lblRate);
+            this.Controls.Add(this.lblHours);
+            this.Controls.Add(this.lblLastname);
+            this.Controls.Add(this.lblFirstname);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblFirstname;
+        private System.Windows.Forms.Label lblLastname;
+        private System.Windows.Forms.Label lblHours;
+        private System.Windows.Forms.Label lblRate;
+        private System.Windows.Forms.Label lblGross;
+        private System.Windows.Forms.TextBox txtFirstname;
+        private System.Windows.Forms.TextBox txtGross;
+        private System.Windows.Forms.TextBox txtRate;
+        private System.Windows.Forms.TextBox txtHours;
+        private System.Windows.Forms.TextBox txtLastname;
+    }
+}
+
